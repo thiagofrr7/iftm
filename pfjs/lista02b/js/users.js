c82bdf0ebcb853d575220504b96b143a0f9d5c19
Thiago Ferreira <thiago.rocha@estudante.iftm.edu.br>
2026-03-03 16:06:08 -0300

    Add usersTable with user data to users.js

---
+++ pfjs/lista02b/js/users.js
@@ -0,0 +1,11 @@
+const usersTable = [
+
+    {nome: "Ana", idade: 18, sexo: 'F', salario: 1000},
+    {nome: "João", idade: 20, sexo: 'M', salario: 3000},
+    {nome: "César", idade: 33, sexo: 'M', salario: 1900},
+    {nome: "Maria", idade: 12, sexo: 'F', salario: 7000},
+    {nome: "Zé", idade: 17, sexo: 'M', salario: 2400}
+
+];
+
+export default usersTable;
