75e5ff7ac7853c4122d4f0ef7de4991d60e79e0b
Thiago <thiago.rocha@estudante.iftm.edu.br>
2026-04-21 19:04:46 -0300

    Updates

--- pfjs/lista5/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Section from "./components/Section";
-import './App.css'
-
-function App() {
-  return <Section />;
-}
-
-export default App;
--- pfjs/lista5/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/iftm/pfjs/lista5/',
-  plugins: [react()],
-})
-
